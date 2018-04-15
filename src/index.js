2aa10ac57c6fc7f10e2faf16118785f1dcf89fdf
vijay <vijay777pawar@gmail.com>
2018-04-16 00:54:53 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-export default (target, thees) => {
-    const preproxy = new WeakMap();
-  
-    const makeHandler = (path) => {
-      return {
-        set(target, key, value, receiver) {
-          if (typeof value === 'object') {
-            value = proxify(value, [...path, key]);
-          }
-          target[key] = value;
-          thees.setState({ [key]: value })
-          return true;
+export const ReactChopper = (target, thees) => {
+  const preproxy = new WeakMap();
+
+  const makeHandler = path => {
+    return {
+      set(target, key, value, receiver) {
+        if (typeof value === "object") {
+          value = proxify(value, [...path, key]);
         }
+        target[key] = value;
+        thees.setState({ [key]: value });
+        return true;
+      }
+    };
+  };
+
+  const proxify = (obj, path) => {
+    for (let key of Object.keys(obj)) {
+      if (typeof obj[key] === "object") {
+        obj[key] = proxify(obj[key], [...path, key]);
       }
     }
-  
-    const proxify = (obj, path) => {
-      for (let key of Object.keys(obj)) {
-        if (typeof obj[key] === 'object') {
-          obj[key] = proxify(obj[key], [...path, key]);
-        }
-      }
-      let p = new Proxy(obj, makeHandler(path));
-      preproxy.set(p, obj);
-      return p;
-    }
-  
-    return proxify(target, []);
-  }
+    let p = new Proxy(obj, makeHandler(path));
+    preproxy.set(p, obj);
+    return p;
+  };
+
+  return proxify(target, []);
+};
+
+export default ReactChopper;
